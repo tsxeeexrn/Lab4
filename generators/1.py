5cb1057e252e91fe68e39e734306b6964a5b8abd
tsxeeexrn <anideit228@gmail.com>
2025-02-12 15:24:48 +0500

    Add files via upload

---
+++ generators/1.py
@@ -0,0 +1,3 @@
+n = int(input())
+for n in range(0, n+1):
+    print(n*n)
---
+++ generators/4.py
@@ -0,0 +1,4 @@
+a = int(input("enter first number area: "))
+b = int(input("enter the end of number are: "))
+for i in range(a, b+1):
+    print(i*i)
